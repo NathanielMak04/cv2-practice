1c10d679890327cfc513cc439e250b87ada48b2e
nathaniel <nathanielmak@outlook.com>
2025-05-13 12:27:36 -0700

    vid capture

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("C:/Users/Nathaniel/Desktop/augmented-reality-app/images/f22.jpg")
+#img = cv2.imread("C:/Users/admin/Desktop/cv-practice/cv2-practice/images/f22.jpg")
+vid = cv2.VideoCapture(0)\
+vid.set(3,640)
+vid.set(4,480)
 
-cv2.imshow("Image", img)
-cv2.waitKey(0)
+#cv2.imshow("Image", img)
+#cv2.waitKey(0)
+
+while True:
+    ret, frame = vid.read()
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 
