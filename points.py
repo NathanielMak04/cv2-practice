fad69d065602eedf9ec3bec101886e2f481f9580
Nathaniel <nathanielmak@outlook.com>
2025-05-28 08:53:42 -0700

    added points.py file - focuses on extracting specific section of the original image

--- points.py
+++ points.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("C:/Users/admin/Desktop/cv-practice/cv2-practice/images/f22.jpg")
+#img = cv2.imread("C:/Users/admin/Desktop/cv-practice/cv2-practice/images/f22.jpg")
+img = cv2.imread("C:/Users/Nathaniel/Desktop/augmented-reality-app/images/f22.jpg")
 
+width,height = 200,200
+pts1 = np.float32([[19,14],[253,14],[19,139],[253,139]])
+pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
 
+matrix = cv2.getPerspectiveTransform(pts1,pts2)
+
+result = cv2.warpPerspective(img,matrix,(width,height))
 
 cv2.imshow("Image", img)
+cv2.imshow("Result", result)
 cv2.waitKey(0)
+
